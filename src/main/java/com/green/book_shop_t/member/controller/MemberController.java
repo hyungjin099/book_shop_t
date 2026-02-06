8a63307bef5c487d384b5763c727aca9a946e801
hyungjin099 <hyungjin099@gmail.com>
2026-02-06 17:45:33 +0900

    레이아웃 작업

---
+++ src/main/java/com/green/book_shop_t/member/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.green.book_shop_t.member.controller;
+
+import com.green.book_shop_t.member.dto.MemberDTO;
+import com.green.book_shop_t.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+  private final MemberService memberService;
+
+
+}
+
+
+
+
+
+
+
---
+++ src/main/java/com/green/book_shop_t/member/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.green.book_shop_t.member.service;
+
+import com.green.book_shop_t.member.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+  private final MemberMapper memberMapper;
+
+}
+
+
+
+
+
+
+
+
+
